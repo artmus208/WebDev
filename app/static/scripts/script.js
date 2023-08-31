aca7feb8df5f74f4551e7e01e6c6591826bd1535
Dmitriy <plotnikovdi95@gmail.com>
2023-08-31 17:47:41 +0300

    Сделаны глвная страница и страница 'Подробный отчет по проекту'

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,11 +1,24 @@
 const lower_date = document.getElementById('lower_date')
 const upper_date = document.getElementById('upper_date')
 function disable_date(checkboxElem) {
-    if (checkboxElem.checked) {
-        lower_date.disabled = true
-        upper_date.disabled = true
-    } else {
-        lower_date.disabled = false
-        upper_date.disabled = false
-    }
+  if (checkboxElem.checked) {
+    lower_date.disabled = true
+    upper_date.disabled = true
+  } else {
+    lower_date.disabled = false
+    upper_date.disabled = false
+  }
 }
+
+// const stage = document.querySelector('.stage')
+const stageHeaders = document.querySelectorAll('.stage__header')
+// const stageBody = document.querySelector('.stage__body')
+
+// stageHeader.addEventListener('click', () => {
+//   stage.classList.toggle('open')
+// })
+stageHeaders.forEach(item => {
+  item.addEventListener('click', () => {
+    item.closest('.stage').classList.toggle('open')
+  })
+})
